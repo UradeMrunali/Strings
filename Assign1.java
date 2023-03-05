4e7e56146198683fdfd8c2fe8ac36004aabc2e90
Mrunali <mrunaliurade26@gmail.com>
2023-03-05 20:00:38 +0530

    Adding string ex

---
+++ Assign1.java
@@ -0,0 +1,20 @@
+
+public class Assign1 {
+      public static void main(String[] args) {
+		
+    	  //By string lateral:
+    	  String s = "Asterisc Computer Institute";
+		
+    	/* By new keyword:
+    	  String s = new String("Asterisc Computer Institute"); */
+		
+		
+		/*By converting Char array into string:
+		char ch []	= {	'A','s','t','e','r','i','s','c' };
+				String s = new String (ch);*/
+    	  
+    	  
+		System.out.println("The character at 0 position is :" + s.charAt(0));
+		System.out.println("The character at 9 position is :" + s.charAt(9));
+	}
+}

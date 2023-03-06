3167532f40130049a2a77b377a67a89b02e64193
Mrunali <mrunaliurade26@gmail.com>
2023-03-06 15:19:30 +0530

    count a no. of unicode

---
+++ Assign2.java
@@ -0,0 +1,11 @@
+
+public class Assign2 {
+
+	public static void main(String[] args) {
+		  
+				String s = "asterisc.in";
+				System.out.println("The Codepoint Count  is :" + s.length());
+				
+	}
+
+}

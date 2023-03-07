87281603301bde92fc0bcccbd916b15d3ae7cf86
Mrunali <mrunaliurade26@gmail.com>
2023-03-07 22:13:18 +0530

    comparing two strings

---
+++ Assign3.java
@@ -0,0 +1,10 @@
+
+public class Assign3 {
+ public static void main(String[] args) {
+	String s1 = " This is Exerceise 1 "; 
+	String s2 = " This is Exerceise 2 "; 
+	
+	System.out.println(s1 + s1.compareTo(s2) + s2);
+}
+ 
+}

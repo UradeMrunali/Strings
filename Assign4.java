433bf6c1d81ed690e9c5e0ae39174ca13c9dcace
Mrunali <mrunaliurade26@gmail.com>
2023-03-08 20:15:15 +0530

    comparing two strings, ignoring case differences

---
+++ Assign4.java
@@ -0,0 +1,27 @@
+
+public class Assign4 {
+	public static void main(String[] args) {
+		
+	
+	String s1 = " This is exerceise 1 "; 
+	String s2 = " This is Exerceise 1 "; 
+	//int result;
+	
+	//result = s1.compareToIgnoreCase(s2) ;
+//
+//	if(result==0)
+//	{
+	System.out.println(s1 + s1.equalsIgnoreCase(s2) + s2);
+//		
+//	}
+//	
+////	else if (result==0) {
+////		System.out.println(s1 + "is equal to" + s2);
+////	}
+//	else {
+//		System.out.println(s1 + "is greater than" + s2);
+//	}
+//	}
+//}
+}
+}

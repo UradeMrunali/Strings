c6575cab1b1012e6c266534f26fb48fef349769e
Mrunali <mrunaliurade26@gmail.com>
2023-03-09 20:32:31 +0530

    concatenating given string to the end of another string

---
+++ Assign5.java
@@ -0,0 +1,11 @@
+
+public class Assign5 {
+	public static void main(String[] args) {
+		String s1 = "PHP Exercises and ";
+		String s2 = "Python Exercises ";
+		
+		System.out.println("The concatenated string is :" + s1.concat(s2));
+		
+	}
+
+}

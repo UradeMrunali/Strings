236f5d0aacc90270311604c11afc43113a8e0e73
Mrunali <mrunaliurade26@gmail.com>
2023-03-10 20:28:58 +0530

    testing if a given string contains the specified sequence of char values

---
+++ Assign6.java
@@ -0,0 +1,12 @@
+
+public class Assign6 {
+ public static void main(String[] args) {
+	String s="PHP Exercises and Python Exercises ";
+	String s1="and";
+	
+	System.out.println("Original sting :" + s);
+	System.out.println("Specified sequence of char values :"+ s1);
+	
+	System.out.println(s.contains(s1));
+}
+}

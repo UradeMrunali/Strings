bfe2e716b7e7557d3ad9dec021b7e19423ceb7b3
Mrunali <mrunaliurade26@gmail.com>
2023-03-11 21:53:57 +0530

    comparing given string to specified character sequence

---
+++ Assign7.java
@@ -0,0 +1,11 @@
+
+public class Assign7 {
+ public static void main(String[] args) {
+	String s1 = "example.com";
+	String s2 = "example.com";
+	String s3 = "Example.com";
+	
+	System.out.println(s1.contains(s2));
+	System.out.println(s3.contains(s1));
+}
+}

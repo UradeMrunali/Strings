34c0e2b0ab897fdf04bf5d69b07a36ded2266610
Mrunali <mrunaliurade26@gmail.com>
2023-03-12 21:52:14 +0530

    checking given string ends with the contents of another string

---
+++ Assign8.java
@@ -0,0 +1,16 @@
+
+public class Assign8 {
+	public static void main(String args[]){
+		String s1 = "Python Exercises";
+		String s2 = "Python Exercise";
+		String end_s = "se";
+		
+//		boolean end1 = s1.endsWith(end_s);
+//		boolean end2 = s2.endsWith(end_s);
+//		
+		System.out.println(s1.endsWith(end_s));
+		System.out.println(s2.endsWith(end_s));
+		
+	}
+
+}

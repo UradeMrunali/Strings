9dfdb48cddf016f8e5e5ba3c19d9cb40631803f8
Mrunali <mrunaliurade26@gmail.com>
2023-03-13 12:35:40 +0530

    checking whether two string objects contain the same data

---
+++ Assign9.java
@@ -0,0 +1,11 @@
+
+public class Assign9 {
+  public static void main(String args[]){
+	  String s1 = "Stephen Edwin King";
+	  String s2 = "Walter Winchell";
+	  String s3 = "Mike Royko";
+	  
+	  System.out.println(s1.equals(s2));
+	  System.out.println(s1.equals(s3));
+  }
+}

3d7737046f541bae02f86de558c1fe4116893af1
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2023-03-13 12:35:21 +0530

    Update Assign9.java

--- Assign9.java
+++ Assign9.java
@@ -2,7 +2,7 @@
 public class Assign9 {
   public static void main(String args[]){
 	 String s1 = "Stephen Edwin King";
-	  String s2 = "Walter Winchell";
+	 String s2 = "Walter Winchell";
 	  String s3 = "Mike Royko";
 	  
 	  System.out.println(s1.equals(s2));

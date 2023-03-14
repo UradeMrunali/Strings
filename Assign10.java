99c414996090ffed733e38323562600e8778af2f
Mrunali <mrunaliurade26@gmail.com>
2023-03-14 19:29:10 +0530

    comparing strings,ignoring case considaration

---
+++ Assign10.java
@@ -0,0 +1,11 @@
+
+public class Assign10 {
+ public static void main(String args[]){
+	 String s1 = "Stephen Edwin King";
+	  String s2 = "Walter Winchell";
+	  String s3 = "stephen edwin king";
+	  
+	  System.out.println(s1.equalsIgnoreCase(s2));
+	  System.out.println(s1.equalsIgnoreCase(s3));
+ }
+}

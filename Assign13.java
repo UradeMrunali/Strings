15a4c34d56186611c25c3111c50eb06f1fdc166a
Mrunali <mrunaliurade26@gmail.com>
2023-03-17 15:52:54 +0530

    getting a substring of given string

---
+++ Assign13.java
@@ -0,0 +1,9 @@
+
+public class Assign13 {
+	public static void main(String args[]){
+		 String s = "The quick brown fox jumps over the lazy dog";
+		 
+		 System.out.println(s);
+		 System.out.println("New String :" + s.substring(10,25));
+	 }
+}

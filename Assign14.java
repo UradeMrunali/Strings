c0a7ccf09c7be5112ee65d7a03f094b758665175
Mrunali <mrunaliurade26@gmail.com>
2023-03-18 22:42:01 +0530

    converting all characters in a string to uppercase

---
+++ Assign14.java
@@ -0,0 +1,9 @@
+
+public class Assign14 {
+	public static void main(String args[]){
+		 String s = " The Quick BroWn FoX!";
+		 
+		 System.out.println(s);
+		 System.out.println("New String :" + s.toUpperCase());
+	 }
+}

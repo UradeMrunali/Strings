cfafc57d8b9cc38995c5bbf8bec6463e91d4d745
Mrunali <mrunaliurade26@gmail.com>
2023-03-19 11:16:13 +0530

    triming whitespace from given string

---
+++ Assign15.java
@@ -0,0 +1,9 @@
+
+public class Assign15 {
+	public static void main(String args[]){
+		 String s = "      Java Exercises    ";
+		 
+		 System.out.println("original :"+s);
+		 System.out.println("New String :"+s.trim());
+	 }
+}

448af381d4f18d8b3351a4af063a2c7b3935e93c
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2023-03-21 21:27:43 +0530

    Update SBuff.java

--- SBuff.java
+++ SBuff.java
@@ -2,8 +2,8 @@
 public class SBuff {
  public static void main(String[] args) {
 
-	 String s1= new String("mrunali");
-	 s1="urade";
+   String s1= new String("mrunali");
+   s1="urade";
 	 
  StringBuffer s= new StringBuffer("hello");
  s.append(" hey i am learning java ");

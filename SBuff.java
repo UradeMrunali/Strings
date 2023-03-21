03606dcaf026ad86b7ceae289051ace0793e7c2b
Mrunali <mrunaliurade26@gmail.com>
2023-03-21 21:27:34 +0530

    Example of string buffer

---
+++ SBuff.java
@@ -0,0 +1,19 @@
+
+public class SBuff {
+ public static void main(String[] args) {
+
+	 String s1= new String("mrunali");
+	 s1="urade";
+	 
+ StringBuffer s= new StringBuffer("hello");
+ s.append(" hey i am learning java ");
+ System.out.println(s.capacity());
+ 
+ System.out.println(s1);
+ System.out.println(s);
+ 
+ }
+ 
+ 
+ 
+}
